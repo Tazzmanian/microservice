9509f93058a8fc8f194f1be89479519217b317b6
Teodor Todorov <banskaliqtabg@gmail.com>
2017-03-09 09:36:12 +0200

    eureka

--- springcloud/reservation-sevice/src/main/java/com/example/ReservationServiceApplication.java
+++ springcloud/reservation-sevice/src/main/java/com/example/ReservationServiceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,6 +22,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ReservationServiceApplication {
 
