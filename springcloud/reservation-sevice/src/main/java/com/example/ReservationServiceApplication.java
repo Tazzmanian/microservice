cb0bdd5fdbd6ab6d61f8805cbe5b3b2da05865f7
Teodor Todorov <banskaliqtabg@gmail.com>
2017-03-09 15:59:04 +0200

    change config data

--- springcloud/reservation-sevice/src/main/java/com/example/ReservationServiceApplication.java
+++ springcloud/reservation-sevice/src/main/java/com/example/ReservationServiceApplication.java
@@ -14,16 +14,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
+@IntegrationComponentScan
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class ReservationServiceApplication {
 
     @Bean
@@ -39,6 +46,18 @@ public class ReservationServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+}
+
+@MessageEndpoint
+class ReservationProcessor {
+
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @ServiceActivator(inputChannel = Sink.INPUT)
+    public void accpetNewReservation(String rn) {
+        this.reservationRepository.save(new Reservation(rn));
     }
 }
 
